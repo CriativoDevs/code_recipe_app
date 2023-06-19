bfb93782f9b4cd1d13fcdac03d1f14fc03588d76
ianderson-pablo-bsurance <102971846+ianderson-pablo-bsurance@users.noreply.github.com>
2023-06-19 21:33:48 +0100

    Adding models on BE

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,16 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cooking', # Sempre coloque seu novo projecto aqui.
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # tem que estar antes do common de django abaixo
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.csrf.csrfViewMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -117,7 +122,26 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/' # Pode ser dado qualquer nome, mas por padrão, coloca-se media.
+MEDIA_ROOT = BASE_DIR/'media' # Aqui com o BASE_DIR onde estiver hospedado, será a base de diretório, e buscará as medias no diterório media.
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://change.allowed.com',
+# ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
